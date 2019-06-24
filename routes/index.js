f6741429163f05c747aca55ca8362426ff7e3b53
gamegine <victor.fromentin95@ynov.com>
2019-06-24 21:37:34 +0200

    ssh test echo

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var config = require('../bin/config');
 var express = require('express');
 var router = express.Router();
 
@@ -6,4 +7,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/server', function(req, res, next) {
+  GetCmdData("echo test").then(function(data){res.render('index', { title: data });})
+});
+
 module.exports = router;
+
+function GetCmdData(cmd){return new Promise(function(res,rej){
+var Client = require('ssh2').Client;
+var conn = new Client();
+conn.on('ready', function() {
+  conn.exec(cmd, function(err, stream) {
+    if (err) throw err;
+    stream.on('data', function(data) {res(""+data)})
+  });}).connect(config.ssh);
+});}
