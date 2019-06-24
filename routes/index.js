ff5886c3165377569df96c6bf5140f3f0c1597e0
gamegine <victor.fromentin95@ynov.com>
2019-06-24 22:56:35 +0200

    get & print data cpu ram mem

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {title: 'Express'});
 });
 
 router.get('/server', function(req, res, next) {
-  GetCmdData("echo test").then(function(data){res.render('index', { title: data });})
+	Promise.all([GetCmdData(cpu), GetCmdData(ram), GetCmdData(mem)]).then(function(data) {
+		var json = {"cpu":JSON.parse(data[0]),"ram":JSON.parse(data[1]),"mem":JSON.parse(data[2])}
+		res.render('index', {title: JSON.stringify(json)});
+        })
 });
 
 module.exports = router;
 
-function GetCmdData(cmd){return new Promise(function(res,rej){
-var Client = require('ssh2').Client;
-var conn = new Client();
-conn.on('ready', function() {
-  conn.exec(cmd, function(err, stream) {
-    if (err) throw err;
-    stream.on('data', function(data) {res(""+data)})
-  });}).connect(config.ssh);
-});}
+function GetCmdData(cmd) {
+    return new Promise(function(res, rej) {
+        var Client = require('ssh2').Client;
+        var conn = new Client();
+        conn.on('ready', function() {
+            conn.exec(cmd, function(err, stream) {
+                if (err) throw err;
+                stream.on('data', function(data) {res("" + data)})
+            });
+        }).connect(config.ssh);
+    });
+}
+
+var cpu ='grep \'cpu \' /proc/stat | awk \'{usage=($2+$4)*100/($2+$4+$5)} END {print "{\\"usage\\":"usage"}"}\''
+var ram='free | grep Mem | awk \'{print "{\\"all\\":"$2",\\"used\\":"$3",\\"usage\\":"($3/$2*100)"}"}\''
+var mem='df -hl | grep dev/sda1 | awk \'END {print "{\\"size\\":\\""$2"\\",\\"used\\":\\""$3"\\",\\"p\\":\\""$5"\\"}"}\''
+
